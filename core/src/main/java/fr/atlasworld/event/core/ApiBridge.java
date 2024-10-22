52d193e2f725d2628da920ae00a39342a730378c
RaftDev <theraft08@gmail.com>
2024-10-22 19:06:24 +0200

    Fixed Missing Service file

--- core/src/main/java/fr/atlasworld/event/core/ApiBridge.java
+++ core/src/main/java/fr/atlasworld/event/core/ApiBridge.java
@@ -2,12 +2,11 @@ package fr.atlasworld.event.core;
 
 import fr.atlasworld.event.api.Event;
 import fr.atlasworld.event.api.EventNode;
-import fr.atlasworld.event.api.internal.ApiBridge;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Predicate;
 
-public class ApiBridgeImpl implements ApiBridge {
+public class ApiBridge implements fr.atlasworld.event.api.internal.ApiBridge {
     @Override
     public <E extends Event> EventNode<E> createEventNode(String name, Class<E> eventType, @Nullable Predicate<E> filter) {
         return new EventNodeImpl<>(name, eventType, filter);
